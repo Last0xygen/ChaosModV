3dbc72f5723cd003b6be9f0e8546c0113bbc95e5
Last0xygen <41595552+Last0xygen@users.noreply.github.com>
2022-04-20 21:02:51 +0200

    InitialCommit

---
+++ VotingOverlayDesigner/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace VotingOverlayDesigner
+{
+    partial class DesignerOverview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DesignerOverview));
+            this.previewWindow = new System.Windows.Forms.WebBrowser();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.propGrid = new System.Windows.Forms.PropertyGrid();
+            this.SuspendLayout();
+            // 
+            // previewWindow
+            // 
+            this.previewWindow.Dock = System.Windows.Forms.DockStyle.Right;
+            this.previewWindow.Location = new System.Drawing.Point(610, 0);
+            this.previewWindow.MaximumSize = new System.Drawing.Size(450, 0);
+            this.previewWindow.MinimumSize = new System.Drawing.Size(450, 0);
+            this.previewWindow.Name = "previewWindow";
+            this.previewWindow.ScrollBarsEnabled = false;
+            this.previewWindow.Size = new System.Drawing.Size(450, 491);
+            this.previewWindow.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(458, 441);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(126, 38);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh Preview";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(326, 441);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(126, 38);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // propGrid
+            // 
+            this.propGrid.CommandsVisibleIfAvailable = false;
+            this.propGrid.Location = new System.Drawing.Point(12, 12);
+            this.propGrid.Name = "propGrid";
+            this.propGrid.PropertySort = System.Windows.Forms.PropertySort.Categorized;
+            this.propGrid.Size = new System.Drawing.Size(572, 423);
+            this.propGrid.TabIndex = 2;
+            // 
+            // DesignerOverview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 491);
+            this.Controls.Add(this.propGrid);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.previewWindow);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1076, 530);
+            this.MinimumSize = new System.Drawing.Size(1076, 530);
+            this.Name = "DesignerOverview";
+            this.Text = "Voting Designer";
+            this.Load += new System.EventHandler(this.DesignerOverview_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser previewWindow;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PropertyGrid propGrid;
+    }
+}
+
---
+++ VotingOverlayDesigner/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace VotingOverlayDesigner
+{
+    public partial class DesignerOverview : Form
+    {
+        public DesignerOverview()
+        {
+            InitializeComponent();
+        }
+
+        private void DesignerOverview_Load(object sender, EventArgs e)
+        {
+            this.loadHtml();
+        }
+
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            this.loadHtml();
+        }
+
+        private void loadHtml()
+        {
+            createCss();
+            string appDir = Path.GetDirectoryName(
+                Assembly.GetExecutingAssembly().GetName().CodeBase);
+            this.previewWindow.Url = new Uri(Path.Combine(appDir, @"Resources/test.html"));
+
+        }
+
+        private void createCss()
+        {
+            string appDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string cssPath = Path.Combine(appDir, "Resources\\testBase.css");
+
+            String css = File.ReadAllText(cssPath);
+
+
+            bool borderEnabled = false;
+
+
+            css = css.Replace("##text-color##", "#ffffff");
+            css = css.Replace("##text-font-family##", "'Arial'");
+            css = css.Replace("##text-font-weight##", "600");
+            css = css.Replace("##text-outline-color##", "#000000");
+            css = css.Replace("##text-outline-thickness##", "1.3px");
+            css = css.Replace("##text-size##", "22pt");
+            css = css.Replace("##bar-background-disabled##", "#afafaf");
+            css = css.Replace("##bar-background##", "#727272");
+            css = css.Replace("##bar-font-size##", "14pt");
+            css = css.Replace("##bar-font-weight##", "600");
+            css = css.Replace("##bar-font##", "'Arial'");
+            css = css.Replace("##bar-height##", "35px");
+            css = css.Replace("##bar-width##", "450px");
+            css = css.Replace("##bar-progression-disabled##", "#8d8d8d");
+            css = css.Replace("##bar-progression##", "#238beb");
+            css = css.Replace("##bar-text-color-disabled##", "#f3f3f3");
+            css = css.Replace("##bar-text-color##", "#f1f1f1");
+            css = css.Replace("##bar-text-outline-color##", "#000000");
+            css = css.Replace("##bar-text-outline-thickness##", "0px");
+            css = css.Replace("##bar-border-radius##", "0px");
+            css = css.Replace("##bar-progression-border-radius##", "0px");
+            css = css.Replace("##bar-border-color##", "#00B4FFf0");
+            css = css.Replace("##border-type##", borderEnabled ? "solid" : "none");
+
+            string testPath = Path.Combine(appDir, "Resources\\test.css");
+
+            File.WriteAllText(testPath, css);
+
+
+
+        }
+
+    }
+}
