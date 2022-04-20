9fa322a2ff70fe9179a51dbdf3eac2b7725de8e7
Last0xygen <41595552+Last0xygen@users.noreply.github.com>
2022-04-20 23:02:29 +0200

    first working version

--- VotingOverlayDesigner/DesignOverview.Designer.cs
+++ VotingOverlayDesigner/DesignOverview.Designer.cs
@@ -37,18 +37,18 @@
             // 
             // previewWindow
             // 
-            this.previewWindow.Dock = System.Windows.Forms.DockStyle.Right;
-            this.previewWindow.Location = new System.Drawing.Point(610, 0);
-            this.previewWindow.MaximumSize = new System.Drawing.Size(450, 0);
-            this.previewWindow.MinimumSize = new System.Drawing.Size(450, 0);
+            this.previewWindow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.previewWindow.Location = new System.Drawing.Point(411, 0);
             this.previewWindow.Name = "previewWindow";
             this.previewWindow.ScrollBarsEnabled = false;
-            this.previewWindow.Size = new System.Drawing.Size(450, 491);
+            this.previewWindow.Size = new System.Drawing.Size(745, 573);
             this.previewWindow.TabIndex = 0;
             // 
             // btnRefresh
             // 
-            this.btnRefresh.Location = new System.Drawing.Point(458, 441);
+            this.btnRefresh.Location = new System.Drawing.Point(279, 441);
             this.btnRefresh.Name = "btnRefresh";
             this.btnRefresh.Size = new System.Drawing.Size(126, 38);
             this.btnRefresh.TabIndex = 1;
@@ -58,7 +58,7 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(326, 441);
+            this.btnSave.Location = new System.Drawing.Point(147, 441);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(126, 38);
             this.btnSave.TabIndex = 1;
@@ -71,21 +71,20 @@
             this.propGrid.Location = new System.Drawing.Point(12, 12);
             this.propGrid.Name = "propGrid";
             this.propGrid.PropertySort = System.Windows.Forms.PropertySort.Categorized;
-            this.propGrid.Size = new System.Drawing.Size(572, 423);
+            this.propGrid.Size = new System.Drawing.Size(393, 423);
             this.propGrid.TabIndex = 2;
             // 
             // DesignerOverview
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1060, 491);
+            this.ClientSize = new System.Drawing.Size(1168, 579);
             this.Controls.Add(this.propGrid);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnRefresh);
             this.Controls.Add(this.previewWindow);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(1076, 530);
-            this.MinimumSize = new System.Drawing.Size(1076, 530);
+            this.MinimumSize = new System.Drawing.Size(732, 524);
             this.Name = "DesignerOverview";
             this.Text = "Voting Designer";
             this.Load += new System.EventHandler(this.DesignerOverview_Load);
--- VotingOverlayDesigner/Resources/CssSettings.cs
+++ VotingOverlayDesigner/Resources/CssSettings.cs
@@ -1,51 +1,214 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace VotingOverlayDesigner.Resources
 {
-    internal class AppSettCssSettingsings
+    internal class CssSettings
     {
 
+        private string textFontFamily = "Arial";
+        private string textColor = "#ffffff";
+        private int textFontWeight = 600;
+        private string textOutlineColor = "#000000";
+        private float textOutlineThickness = 1.3f;
+        private int textSize = 22;
 
-        private bool saveOnClose = true;
-        private string greetingText = "Welcome to your application!";
-        private int itemsInMRU = 4;
-        private int maxRepeatRate = 10;
-        private bool settingsChanged = false;
-        private string appVersion = "1.0";
+        private string barBackgroundDisabledColor = "#afafaf";
+        private string barBackgroundColor = "#727272";
+        private string barFontSize = "14";
+        private string barFontWeight = "600";
+        private string barFont = "Arial";
+        private string barHeight = "35";
+        private string barWidth = "450";
+        private int barBorderRadius = 0;
+        private string barTextColorDisabled = "#f3f3f3";
+        private string barTextColor = "#f1f1f1";
+        private string barTextOutlineColor = "#000000";
+        private int barTextOutlineThickness = 0;
+        private string barBorderColor = "#00B4FFf0";
+        private bool barBorderEnabled = false;
 
-        public bool SaveOnClose
+        private string barProgressionDisabledColor = "#8d8d8d";
+        private string barProgressionColor = "#238beb";
+        private int barProgressionBorderRadius = 0;
+
+
+        #region Total Votes
+
+        [CategoryAttribute("Total Votes Text Settings"), DescriptionAttribute("The font family to be used, has to be installed on the computer")]
+        public string TextFontFamily
         {
-            get { return saveOnClose; }
-            set { saveOnClose = value; }
+            get { return textFontFamily; }
+            set { textFontFamily = value; }
         }
-        public string GreetingText
+
+        [CategoryAttribute("Total Votes Text Settings"), DescriptionAttribute("The color to use for the text")]
+        public string TextColor
         {
-            get { return greetingText; }
-            set { greetingText = value; }
+            get { return textColor; }
+            set { textColor = value; }
         }
-        public int MaxRepeatRate
+
+        [CategoryAttribute("Total Votes Text Settings"), DescriptionAttribute("The text font weight")]
+        public int TextFontWeight
         {
-            get { return maxRepeatRate; }
-            set { maxRepeatRate = value; }
+            get { return textFontWeight; }
+            set { textFontWeight = value; }
         }
-        public int ItemsInMRUList
+
+        [CategoryAttribute("Total Votes Text Settings"), DescriptionAttribute("The color to use for the text outline")]
+        public string TextOutlineColor
         {
-            get { return itemsInMRU; }
-            set { itemsInMRU = value; }
+            get { return textOutlineColor; }
+            set { textOutlineColor = value; }
         }
-        public bool SettingsChanged
+
+        [CategoryAttribute("Text Settings"), DescriptionAttribute("The thickness of text outline")]
+        public float TextOutlineThickness
         {
-            get { return settingsChanged; }
-            set { settingsChanged = value; }
+            get { return textOutlineThickness; }
+            set { textOutlineThickness = value; }
         }
-        public string AppVersion
+
+        [CategoryAttribute("Text Settings"), DescriptionAttribute("The text size... you know, the size of the text.")]
+        public int TextSize
         {
-            get { return appVersion; }
-            set { appVersion = value; }
+            get { return textSize; }
+            set { textSize = value; }
         }
+
+        #endregion
+
+        #region Bar
+
+        [CategoryAttribute("Bar Settings"), DescriptionAttribute("The color to use for a disabled bar")]
+        public string BarBackgroundDisabledColor
+        {
+            get { return barBackgroundDisabledColor; }
+            set { barBackgroundDisabledColor = value; }
+        }
+
+        [CategoryAttribute("Bar Settings"), DescriptionAttribute("The color to use for the bar background")]
+        public string BarBackgroundColor
+        {
+            get { return barBackgroundColor; }
+            set { barBackgroundColor = value; }
+        }
+
+        [CategoryAttribute("Bar Settings"), DescriptionAttribute("The font size used within the bars")]
+        public string BarFontSize
+        {
+            get { return barFontSize; }
+            set { barFontSize = value; }
+        }
+
+        [CategoryAttribute("Bar Settings"), DescriptionAttribute("The font weight used within the bars")]
+        public string BarFontWeight
+        {
+            get { return barFontWeight; }
+            set { barFontWeight = value; }
+        }
+
+        [CategoryAttribute("Bar Settings"), DescriptionAttribute("The font family to be used, has to be installed on the computer")]
+        public string BarFont
+        {
+            get { return barFont; }
+            set { barFont = value; }
+        }
+
+        [CategoryAttribute("Bar Settings"), DescriptionAttribute("The height of each bar")]
+        public string BarHeight
+        {
+            get { return barHeight; }
+            set { barHeight = value; }
+        }
+
+        [CategoryAttribute("Bar Settings"), DescriptionAttribute("The width of all bars")]
+        public string BarWidth
+        {
+            get { return barWidth; }
+            set { barWidth = value; }
+        }
+
+        [CategoryAttribute("Bar Settings"), DescriptionAttribute("The border radius to use for all bars")]
+        public int BarBorderRadius
+        {
+            get { return barBorderRadius; }
+            set { barBorderRadius = value; }
+        }
+
+        [CategoryAttribute("Bar Settings"), DescriptionAttribute("The text color if bar is disabled")]
+        public string BarTextColorDisabled
+        {
+            get { return barTextColorDisabled; }
+            set { barTextColorDisabled = value; }
+        }
+
+        [CategoryAttribute("Bar Settings"), DescriptionAttribute("The text color within the bars")]
+        public string BarTextColor
+        {
+            get { return barTextColor; }
+            set { barTextColor = value; }
+        }
+
+        [CategoryAttribute("Bar Settings"), DescriptionAttribute("The outline color for text within the bars")]
+        public string BarTextOutlineColor
+        {
+            get { return barTextOutlineColor; }
+            set { barTextOutlineColor = value; }
+        }
+
+        [CategoryAttribute("Bar Settings"), DescriptionAttribute("The outline thickness for text within the bars")]
+        public int BarTextOutlineThickness
+        {
+            get { return barTextOutlineThickness; }
+            set { barTextOutlineThickness = value; }
+        }
+
+        [CategoryAttribute("Bar Settings"), DescriptionAttribute("The border color for the bars")]
+        public string BarBorderColor
+        {
+            get { return barBorderColor; }
+            set { barBorderColor = value; }
+        }
+
+        [CategoryAttribute("Bar Settings"), DescriptionAttribute("Enabled Borders for the bars")]
+        public bool BarBorderEnabled
+        {
+            get { return barBorderEnabled; }
+            set { barBorderEnabled = value; }
+        }
+        #endregion
+
+        #region Progress Bar
+
+
+        [CategoryAttribute("Progress Bar Settings"), DescriptionAttribute("The color to use for a disabled progress bar")]
+        public string BarProgressionDisabledColor
+        {
+            get { return barProgressionDisabledColor; }
+            set { barProgressionDisabledColor = value; }
+        }
+
+        [CategoryAttribute("Progress Bar Settings"), DescriptionAttribute("The color to use for a progress bar")]
+        public string BarProgressionColor
+        {
+            get { return barProgressionColor; }
+            set { barProgressionColor = value; }
+        }
+
+        [CategoryAttribute("Progress Bar Settings"), DescriptionAttribute("The border radius to use for a progress bar")]
+        public int BarProgressionBorderRadius
+        {
+            get { return barProgressionBorderRadius; }
+            set { barProgressionBorderRadius = value; }
+        }
+
+        #endregion
     }
 }
